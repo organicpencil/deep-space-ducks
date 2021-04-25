239e2979a3568fb01a7003ac5baf84da7fdc0b8d
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2021-04-25 09:18:08 -0500

    Add boost

--- project/player/player.gd
+++ project/player/player.gd
@@ -30,8 +30,12 @@ func _unhandled_input(event):
 			yield(get_tree().create_timer(0.1), "timeout")
 
 func _physics_process(delta):
-	add_central_force(global_transform.basis * Vector3(0.0, 0.0, -60.0))
-	
+	var boost = 1.0
+	if Input.is_action_pressed("boost"):
+		boost = 3.0
+		
+	add_central_force(global_transform.basis * Vector3(0.0, 0.0, -40.0) * boost)
+
 	var v = 0.0
 	
 	if Input.is_action_pressed("left"):
@@ -40,4 +44,4 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		v -= 3.0
 		
-	angular_velocity = Vector3(0, v, 0)
+	angular_velocity = Vector3(0, v, 0) / boost
