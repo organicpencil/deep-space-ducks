d2754c22401fdf4d6bc2932efc83dffa3ae330b7
austinirvine <aa1irvine@gmail.com>
2021-04-25 08:15:19 -0500

    ducks can have no hats

--- project/models/DuckyHat.gd
+++
@@ -1,7 +0,0 @@
-extends MeshInstance
-
-# load resource hats
-
-func _ready():
-	#instantiate random hats
-	pass # Replace with function body.
---
+++ project/models/ducky-hat.gd
@@ -0,0 +1,21 @@
+extends Spatial
+
+func _ready():
+	var hat_array = ["funhat.tscn", "blackhat.tscn", "cowboyhat.tscn", ""]
+	var index = randi() % 4
+	
+	var hat = null
+	if hat_array[index] != "":
+		hat = load("res://Models/hats/" + hat_array[index]).instance()
+	
+	if hat != null:
+		if (hat_array[index] == "blackhat.tscn"):
+			hat.translation = Vector3(0.0, 1.145, 0.0)
+		elif (hat_array[index] == "funhat.tscn"):
+			hat.translation = Vector3(0.004, 0.986, 0.02)
+			hat.rotation = Vector3(-11.13, 0.065, -0.127)
+			hat.scale = Vector3(0.8, 0.8, 0.8)
+		else:
+			hat.translation = Vector3(0.006, 0.993, -0.172)
+			hat.rotation = Vector3(0.102, 89.9, -11.77)
+		add_child(hat)
