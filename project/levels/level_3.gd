c7f937f83f94e58ffb28edaa902499c1044011be
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2021-04-26 18:50:24 -0500

    Level 3 tweaks

--- project/levels/level_3.gd
+++ project/levels/level_3.gd
@@ -1,62 +1,22 @@
 extends "res://levels/level.gd"
 
 
-var enemies_remaining = 0
 var beacons = 4
 
 func _ready():
-	var enemy = spawn_big_enemy()
-	enemy.connect("dead", self, "_wave_2")
-	
 	spawn_beacon()
 	
 func spawn_beacon():
 	if beacons > 0:
+		for i in range(0, (5-beacons)*2):
+			spawn_big_enemy()
+			
 		beacons -= 1
 		var b = preload("res://models/beacon/beacon.tscn").instance()
 		add_child(b)
 		b.connect("placed", self, "spawn_beacon")
 		var pos = $Player.transform.origin + Vector3(rand_range(-100, 100), 0, rand_range(-200, -400))
 		b.transform.origin = pos
-		
+
 	else:
 		Global.emit_signal("win")
-
-func _wave_2():
-	yield(get_tree().create_timer(3.0), "timeout")
-	var enemy = spawn_big_enemy()
-	enemy.connect("dead", self, "_wave_3")
-	enemies_remaining += 1
-	
-	enemy = spawn_big_enemy()
-	enemy.connect("dead", self, "_wave_3")
-	enemies_remaining += 1
-	
-func _wave_3():
-	enemies_remaining -= 1
-	if enemies_remaining == 0:
-		var enemy = spawn_big_enemy()
-		enemy.connect("dead", self, "_wave_4")
-		enemies_remaining += 1
-		
-		enemy = spawn_big_enemy()
-		enemy.connect("dead", self, "_wave_4")
-		enemies_remaining += 1
-		
-func _wave_4():
-	enemies_remaining -= 1
-	if enemies_remaining == 0:
-		var enemy = spawn_big_enemy()
-		enemy.connect("dead", self, "_wave_5")
-		enemies_remaining += 1
-		
-		enemy = spawn_big_enemy()
-		enemy.connect("dead", self, "_wave_5")
-		enemies_remaining += 1
-		
-		enemy = spawn_big_enemy()
-		enemy.connect("dead", self, "_wave_5")
-		enemies_remaining += 1
-
-func _wave_5():
-	pass
