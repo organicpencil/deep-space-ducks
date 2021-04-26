5145ce246e9cdbefc5f977fc17eb81c98b6d7500
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2021-04-26 12:48:01 -0500

    Hide ducky preview when there are no ducks

--- project/ducky_preview.gd
+++ project/ducky_preview.gd
@@ -8,15 +8,20 @@ func _ducky_changed(ducky_type):
 	match ducky_type:
 		Global.DUCKY_NONE:
 			texture = preload("res://duckling/nothing.png")
+			hide()
 			
 		Global.DUCKY_BALD:
 			texture = preload("res://duckling/ducky.png")
+			show()
 			
 		Global.DUCKY_FUNHAT:
 			texture = preload("res://duckling/ducky_funhat.png")
+			show()
 			
 		Global.DUCKY_COWBOYHAT:
 			texture = preload("res://duckling/ducky_cowboyhat.png")
+			show()
 			
 		Global.DUCKY_BLACKHAT:
 			texture = preload("res://duckling/ducky_blackhat.png")
+			show()
