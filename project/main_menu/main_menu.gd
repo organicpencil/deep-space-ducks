4b6a778efdd908df864b0f9fba9f925c65e1bc5e
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2021-04-26 12:46:08 -0500

    Start game with fade transition by pressing any button on main menu

--- project/main_menu/main_menu.gd
+++ project/main_menu/main_menu.gd
@@ -1,5 +1,14 @@
 extends Control
 
+var LEVEL = preload("res://Level.tscn")
 
 func _ready():
 	$StartTexture/AnimationPlayer.play("start_pulse")
+	$AnimationPlayer.connect("animation_finished", self, "_start_game")
+
+func _input(event):
+	if event.is_pressed():
+		$AnimationPlayer.play("transition")
+
+func _start_game(anim):
+	get_tree().change_scene_to(LEVEL)
