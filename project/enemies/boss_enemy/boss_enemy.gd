6abebdf6d9e24f09200731804d8c666ca7a5e42e
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2021-04-26 18:12:40 -0500

    Remove enemy spawner direction indicators, the direction of incoming enemies should fill that role

--- project/enemies/boss_enemy/boss_enemy.gd
+++ project/enemies/boss_enemy/boss_enemy.gd
@@ -13,8 +13,9 @@ func _ready():
 	rotate_y(rand_range(0.0, 6.0))
 	$SpawnTimer.connect("timeout", self, "spawn")
 	
-	$VisibilityNotifier.connect("screen_entered", $Arrow, "hide")
-	$VisibilityNotifier.connect("screen_exited", $Arrow, "show")
+	#$VisibilityNotifier.connect("screen_entered", $Arrow, "hide")
+	#$VisibilityNotifier.connect("screen_exited", $Arrow, "show")
+	$Arrow.hide()
 	
 func take_damage(amount : int):
 	if health > 0:
@@ -32,9 +33,11 @@ func spawn():
 	get_parent().add_child(enemy)
 	enemy.global_transform.origin = global_transform.origin
 
+"""
 func _physics_process(delta):
 	var follow_target = get_node(follow_target_nodepath)
 
 	var v = (global_transform.origin - follow_target.global_transform.origin).normalized()
 	v *= 20.0
 	$Arrow.global_transform.origin = follow_target.global_transform.origin + v
+"""
