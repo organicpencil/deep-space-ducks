e00d5610e70457aee166a712e15e211217d96ee7
austinirvine <aa1irvine@gmail.com>
2021-04-26 14:59:59 -0500

    Update crt config

---
+++ project/crt-corner-button.gd
@@ -0,0 +1,24 @@
+extends Button
+
+export(NodePath) var help_text
+export(NodePath) var health_bar
+export(NodePath) var player_energy
+
+func _ready():
+	help_text = get_node(help_text)
+	health_bar = get_node(health_bar)
+	player_energy = get_node(player_energy)
+
+func _on_PowerButton_power_clicked():
+	help_text.show()
+	health_bar.show()
+	player_energy.show()
+	if (name == "Small CRT Button"):
+		show()
+
+func _on_Small_CRT_Button_pressed():
+	help_text.hide()
+	health_bar.hide()
+	player_energy.hide()
+	if (name == "Big CRT Button"):
+		show()
---
+++ project/tv/main-crt-button.gd
@@ -0,0 +1,11 @@
+extends StaticBody
+
+signal power_clicked
+
+func _ready():
+	connect("input_event", self, "on_input_event")
+
+func on_input_event(camera, event, click_position, click_normal, shape_idx):
+	var mouse_click = event as InputEventMouseButton
+	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
+		emit_signal("power_clicked")
