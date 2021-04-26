2cb5e6a32e7aa1e5f7d7f01cad6ed3b0051d81c4
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2021-04-26 13:32:02 -0500

    Toggle fullscreen with alt+enter

--- project/global.gd
+++ project/global.gd
@@ -9,3 +9,8 @@ signal lose
 enum{DUCKY_FUNHAT, DUCKY_BLACKHAT, DUCKY_COWBOYHAT, DUCKY_BALD, DUCKY_NONE}
 
 var player_path
+
+
+func _input(event):
+	if event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
