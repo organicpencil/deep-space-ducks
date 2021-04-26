b44c22567b17064a770fe5bb02e025ade57fc9ed
austinirvine <aa1irvine@gmail.com>
2021-04-25 22:07:56 -0500

    Add explosions to lazersgst muahahah

--- project/weapons/DuckLaser.gd
+++ project/weapons/DuckLaser.gd
@@ -1,6 +1,5 @@
 extends Area
 
-
 func _ready():
 	$Timer.connect("timeout", self, "queue_free")
 	$Timer.start()
@@ -10,7 +9,10 @@ func _ready():
 func _body_entered(node):
 	if node.has_method("take_damage"):
 		node.take_damage(1)
-		
+	var explosion = load("res://weapons/explosions/explosion.tscn").instance()
+	explosion.transform = node.transform
+	explosion.translate(Vector3(0, 2, 0))
+	get_parent().add_child(explosion)
 	queue_free()
 
 func _physics_process(delta):
